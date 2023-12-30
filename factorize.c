5d6684a72e98800b008754be6cf84ecf25bd7d20
KenNyagz <kennyagz681@gmail.com>
2023-12-30 16:28:06 +0300

    factorize

--- factorize.c
+++ factorize.c
@@ -1,34 +1,35 @@
 #include <stdio.h>
 
 void factorize(int n){
-int i, factor;
+	int i, factor;
 
-/*find first factor*/
-for (int i = 2; i <= n; i++)
-{
-	if (n % i == 0)
+	printf("%d=", n);
+	/*find first factor*/
+	for (int i = 2; i <= n; i++)
 	{
-		factor = i;
-		printf("%d=%d*", n, i);
+		if (n % i == 0)
+		{
+			factor = i;
+			printf("%d*", i);
 		
-		n /= i;
-		i = 1;
+			n /= i;
+			i = 1;
+		}
 	}
-}
-printf("%d\n", i);
+	printf("\b \n");
 }
 
 
 int main(int argc, char *argv[])
 {
-int num;
+	int num;
 
-FILE *file = fopen(argv[1], "r");
+	FILE *file = fopen(argv[1], "r");
 
-while(fscanf(file, "%d", &num) == 1)
-	factorize(num);
+	while(fscanf(file, "%d", &num) == 1)
+		factorize(num);
 
-fclose(file);
+	fclose(file);
 
-return (0);
+	return (0);
 }
